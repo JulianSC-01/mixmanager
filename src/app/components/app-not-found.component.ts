67ec206f9dec603dce79160e32bb276a1f51a1dd
JulianSC-01 <julian.r.salati@gmail.com>
2021-07-24 13:16:40 -0400

    Accessibility Improvements (Not Found)

--- src/app/components/app-not-found.component.ts
+++ src/app/components/app-not-found.component.ts
@@ -1,9 +1,15 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
+import { AppFocusService } from '../services/app-focus.service';
 
 @Component({
   selector: 'app-not-found',
   templateUrl: './app-not-found.component.html'
 })
-export class AppNotFoundComponent {
-  constructor() { }
+export class AppNotFoundComponent implements AfterViewInit {
+  constructor(
+    private focusService : AppFocusService) { }
+
+  ngAfterViewInit() : void {
+    this.focusService.focusMainHeader();
+  }
 }
